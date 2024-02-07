e9e04794f0abd923563dfd9857b2782c74fbbbf1
jahndev <jahnarellano85.dev@gmail.com>
2024-02-07 10:35:47 -0300

    Add GeneratedValue for id field in Client class

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -1,13 +1,12 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="clients")
 public class Client {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     
